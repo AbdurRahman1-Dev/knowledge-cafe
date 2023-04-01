1e0ce2308160c1e1c1338f48d59783a87642e7af
--add <rahmanaky521@gmail.com>
2023-03-31 22:49:36 -0500

    bookmark function added

--- src/components/Bookmark/Bookmark.jsx
+++ src/components/Bookmark/Bookmark.jsx
@@ -3,18 +3,21 @@ import React, { useState } from 'react';
 let arr = []
 
 const Bookmark = (blogList) => {
-  let mainBlog = arr.filter((e, index) => {
-    return arr.indexOf(e) === index;
-  })
-  console.log(mainBlog);
 arr.push(blogList.blog)
+let mainBlog = arr.filter((e, index) => {
+    return arr.indexOf(e) === index;
+})
+mainBlog.splice(0,1)
+// console.log(mainBlog);
+// if()
+
   return (
     <div className='bookmark-sec'>
       <div>
         <h1 >spend time</h1>
       </div>
       <div className='bookmarkBg p-3'>
-        <h4 className='fw-bold '>Bookmarked Blogs: {mainBlog.length - 1}</h4>
+        <h4 className='fw-bold '>Bookmarked Blogs: {mainBlog.length }</h4>
         {
           mainBlog.map((blogs)=> {
             return <p className='bg-light p-2 rounded-3 fw-bold'> {blogs.blogTitle}</p>
